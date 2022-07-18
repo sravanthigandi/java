25726e0b7bcb7d793fea13b09ee92818d21fd5c7
sravanthigandi <99851829+sravanthigandi@users.noreply.github.com>
2022-07-18 19:44:04 +0530

    Add files via upload

---
+++ TwoSum.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+import java.util.HashMap;
+    
+    
+class solution{
+    public static int[] twosum(int[]nums,int target){
+
+       
+        HashMap<Integer , Integer> map = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            map.put(nums[i],i);
+        }
+        for(int i=0;i<nums.length;i++){
+
+        
+            int diff= target-nums[i];
+            if(map.containsKey(diff) && map.get(diff)!=i ){
+                return new int[]{map.get(diff),i};
+
+            }
+        }
+        
+
+        return nums;
+    }
+}
+public class TwoSum {
+    
+    public static void main(String args[]) {
+    
+        int target = 3;
+        int nums[] = {2, 7, 11, 15};
+        nums = solution.twosum(nums, target);
+
+        System.out.println(Arrays.toString(nums));
+    }
+}
